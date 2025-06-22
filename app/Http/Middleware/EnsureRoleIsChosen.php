d58c8bb82c7b444d5409d102e1dfa51ef677b368
CoderTX1 <coder1.travelx@gmail.com>
2025-06-21 23:13:48 -0700

    make logout if dont have account

--- app/Http/Middleware/EnsureRoleIsChosen.php
+++ app/Http/Middleware/EnsureRoleIsChosen.php
@@ -8,18 +8,17 @@ use Symfony\Component\HttpFoundation\Response;
 
 class EnsureRoleIsChosen
 {
-    public function handle(Request $request, Closure $next): Response
-    {
-        if (auth()->check()) {
-            // وإذا ما فيه دور محفوظ
-            if (! session()->has('active_role')) {
-                // نتأكد أننا مش في صفحة اختيار الدور نفسها (عشان ما نعمل redirect دائم)
-                if (! $request->is('admin/choose-role')) {
-                    return redirect('/admin/choose-role');
-                }
+public function handle(Request $request, Closure $next): Response
+{
+    if (auth()->check()) {
+        if (!session()->has('active_role')) {
+            if (! $request->is('admin/choose-role') && ! $request->is('admin/logout')) {
+                return redirect('/admin/choose-role');
             }
         }
-
-        return $next($request);
     }
+
+    return $next($request);
+}
+
 }
